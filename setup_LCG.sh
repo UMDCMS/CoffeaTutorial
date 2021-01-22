666e351ec22722241bd6e9f2ae04ab04279c99ea
Enoch Chen <enochnotsocool@gmail.com>
2021-01-22 12:50:51 -0500

    Added back the lines that alter the activation script

--- setup_LCG.sh
+++ setup_LCG.sh
@@ -15,13 +15,14 @@ source $NAME/bin/activate
 
 echo "\nInstalling 'pip' packages ... "
 python -m pip install --no-cache-dir setuptools pip --upgrade
+python -m pip install --no-cache-dir uproot4
 python -m pip install --no-cache-dir coffea[dask]
 
 # Setup the activation script for the virtual environment
 echo "\nSetting up the activation script for the virtual environment ... "
 sed -i '40s/.*/VIRTUAL_ENV="$(cd "$(dirname "$(dirname "${BASH_SOURCE[0]}" )")" \&\& pwd)"/' $NAME/bin/activate
 find coffeaenv/bin/ -type f -print0 | xargs -0 -P 4 sed -i '1s/#!.*python$/#!\/usr\/bin\/env python/'
-#sed -i "2a source ${LCG}/setup.sh" $NAME/bin/activate
-#sed -i "4a source ${LCG}/setup.csh" $NAME/bin/activate.csh
+sed -i "2a source ${LCG}/setup.sh"  $NAME/bin/activate
+sed -i "4a source ${LCG}/setup.csh" $NAME/bin/activate.csh
 deactivate
 echo "\nFINISHED"
